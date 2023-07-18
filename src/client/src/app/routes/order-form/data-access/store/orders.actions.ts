0cfbbe367dbc218188ce0b778b9fadbcb53f024f
Chris McCown <mccow002@gmail.com>
2023-07-18 12:44:30 -0500

    added delete and update actions

--- src/client/src/app/routes/order-form/data-access/store/orders.actions.ts
+++ src/client/src/app/routes/order-form/data-access/store/orders.actions.ts
@@ -1,5 +1,6 @@
 import { createActionGroup, emptyProps, props } from "@ngrx/store";
 import { Order } from "../models";
+import { Update } from '@ngrx/entity';
 
 export const OrderActions = createActionGroup({
   source: 'Orders',
@@ -7,8 +8,19 @@ export const OrderActions = createActionGroup({
     'Load Orders': emptyProps(),
     'Load Orders Success': props<{ orders: Order[] }>(),
     'Create Order': props<{ assemblyName: string }>(),
-    'Create Order Success': props<{ assemblyName: string, tempId: number }>(),
+    'Create Order Optimistic Update': props<{ assemblyName: string, tempId: number }>(),
+    'Create Order Error': props<{ tempId: number }>(),
+
+    'Edit Order': props<{ orderId: number, assemblyName: string }>(),
+    'Edit Order Optimistic Update': props<{ order: Order }>(),
+    'Edit Order Error': props<{ order: Order }>(),
+
+    'Delete Order': props<{ orderId: number }>(),
+    'Delete Order Optimistic Update': props<{ orderId: number }>(),
+    'Delete Order Error': props<{ order: Order }>(),
+
     'Add Order': props<{ order: Order, tempId?: number }>(),
-    'Add Order Error': props<{ tempId: number }>()
+    'Update Order': props<{ changes: Update<Order> }>(),
+    'Remove Order': props<{ orderId: number }>()
   }
 });
